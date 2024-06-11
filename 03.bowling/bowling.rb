73208450f473863a411a2ef0b68cc1a06933c0ec
hiromachi <hrmykh@gmail.com>
2024-06-11 23:48:27 +0900

    不要なコメントを削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 # 全投球を受け取る
 def convert_shots(result)
   # 1投毎に分割し、Xは10に変換する
@@ -12,10 +10,6 @@ def convert_shots(result)
       shots << s.to_i
     end
   end
-
-  puts "===全投球を受け取りました！==="
-  puts "shots: #{shots}"
-  puts
 
   return shots
 end
@@ -40,17 +34,12 @@ def separate_frames(shots)
     frames[-1] += shots[shot_count..-1]
   end
 
-  puts "===フレーム毎に区切ります！==="
-  puts "フレーム毎の投球結果：#{frames}"
-  puts
-
   return frames
 end
 
 def calc_point(frames)
   point = 0
   frames.each_with_index do |frame, index|
-    puts "frame: #{frame}"
     # 1〜9フレーム目以外の計算
     if index < 9
       # ストライクの場合
@@ -62,14 +51,10 @@ def calc_point(frames)
           point += frames[index + 1][0]
           # 次のフレームがストライクでない場合
           if frames[index + 1][1] != nil
-            puts "aaaaaaa"
-            puts
             # 次のフレームの2投目を加算
             point += frames[index + 1][1]
           # 次のフレームがストライクの場合
           elsif frames[index + 2] != nil
-            puts "bbbbbbb"
-            puts
             # 次のフレームの1投目を加算
             point += frames[index + 2][0]
           end
@@ -79,29 +64,17 @@ def calc_point(frames)
         point += 10
         # 次のフレームの1投目を加算
         if frames[index + 1] != nil
-          puts "ccccccc"
-          puts
           point += frames[index + 1][0]
         end
       # ストライクでもスペアでもない場合
       else
-        puts "ddddddd"
-        puts
         point += frame.sum
       end
     # 10フレーム目の計算
     else
-      puts "eeeeeee"
-      puts
       point += frame.sum
     end
-
-    puts "point: #{point}"
   end
-
-  puts "===結果計算はこちらです！==="
-  puts "合計得点：#{point}"
-  puts
 
   return point
 end
@@ -109,5 +82,4 @@ end
 shots = convert_shots(ARGV[0])
 frames = separate_frames(shots)
 point = calc_point(frames)
-puts "===結果==="
 puts point
